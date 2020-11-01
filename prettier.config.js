660226ead83fe360850cb6bd47ca400fc22c7aef
“andrew <ask@hyliancoder.com>
2020-11-01 05:14:45 -0700

    init git commit redwood

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  useTabs: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  overrides: [
+    {
+      files: 'Routes.js',
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
+};
