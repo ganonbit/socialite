97398b81a08c779a095abdaedc495d27a8ba8bfb
hyliancoder <ask@hyliancoder.com>
2020-11-04 02:38:19 -0700

    added sentry

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,26 @@
 import ReactDOM from 'react-dom';
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web';
 import { ChakraProvider } from '@chakra-ui/core';
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: process.env.SENTRY_TOKEN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
+
 import FatalErrorPage from 'src/pages/FatalErrorPage';
-
 import Routes from 'src/Routes';
-
 import customTheme from 'src/theme';
 
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={customTheme}>
         <Routes />
       </ChakraProvider>
     </RedwoodProvider>
