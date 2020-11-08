8205750a31dc3d99cc1a332909424569a558b53b
hyliancoder <ask@hyliancoder.com>
2020-11-08 13:20:59 -0700

    apparently it wasnt even the themeUI present that made it work. it seems to randomly work at times after package.json yarn.lock is changed? what is the bug?

--- web/src/theme/theme.js
+++ web/src/theme/theme.js
@@ -1,7 +1,5 @@
 import { extendTheme } from '@chakra-ui/core';
-import { tailwind } from '@theme-ui/presets';
 const config = {
-  ...tailwind,
   useSystemColorMode: false,
   initialColorMode: 'dark',
 };
